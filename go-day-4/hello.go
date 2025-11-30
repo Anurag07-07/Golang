1ca68695936c61d2f06d8a45edaa3c1a26cba2c6
Anurag07-07 <adityanv8@gmail.com>
2025-11-30 21:57:55 +0530

    interface and types assertion in  golang

--- go-day-4/hello.go
+++ go-day-4/hello.go
@@ -1,15 +1,17 @@
 ////////////////////////////Interfaces/////////////////////////////
 
 // Interafaces are collections of methods signatures.A type "implements" an interface
-// if it has all of the methods of the given interface defined on it 
+// if it has all of the methods of the given interface defined on it
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type shape interface{
 	area() float64
-	peri() float64
+	pari() float64
 }
 
 type rect struct{
@@ -24,10 +26,44 @@ func (r rect) pari() float64{
 	return  2*r.width+2*r.height
 }
 
-func main(){
-		Rect := rect{}
-		Rect.width = 2.56
-		Rect.height = 5.63
-		fmt.Println("The area is ",Rect.area())	
-		fmt.Println("The Perimeter is ",Rect.pari())	
+// func main(){
+// 		Rect := rect{}
+// 		Rect.width = 2.56
+// 		Rect.height = 5.63
+
+// 		//Using interaface
+// 		var s shape = Rect    
+// 		fmt.Println("The Area is ",s.area())
+// 		fmt.Println("The Perimeter is",s.pari())
+// }
+
+/////////////////////////////////////////////Type Assertions/////////////////////////////////////////////////////////////////////////
+
+type shape1 interface {
+	area1() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+// Method for circle
+func (c circle) area1() float64 {
+	return 3.14 * c.radius * c.radius  
+}
+
+func main() {
+	Circle := circle{radius: 5}
+
+	var s shape1 = Circle
+
+	fmt.Println("Area:", s.area1())
+
+	// Type Assertion (interface -> concrete type)
+	c, ok := s.(circle)
+	if ok {
+		fmt.Println("Radius is:", c.radius)
+	} else {
+		fmt.Println("Type assertion failed")
+	}
 }
